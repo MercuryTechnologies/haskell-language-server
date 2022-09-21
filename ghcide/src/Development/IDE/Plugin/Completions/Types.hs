6c47b28f4e15ca44f2dbf0ec07ca90957965aeed
Zubin Duggal <zubin.duggal@gmail.com>
2022-09-21 10:14:04 +0530

    Implement completionItem/resolve

--- ghcide/src/Development/IDE/Plugin/Completions/Types.hs
+++ ghcide/src/Development/IDE/Plugin/Completions/Types.hs
@@ -11,7 +11,8 @@ import           Control.DeepSeq
 import qualified Data.Map                     as Map
 import qualified Data.Text                    as T
 
-import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Aeson
+import           Data.Aeson.Types
 import           Data.Hashable                (Hashable)
 import           Data.Text                    (Text)
 import           Data.Typeable                (Typeable)
@@ -22,6 +23,7 @@ import           GHC.Generics                 (Generic)
 import           Ide.Plugin.Properties
 import           Language.LSP.Types           (CompletionItemKind (..), Uri)
 import qualified Language.LSP.Types           as J
+import qualified GHC.Types.Name.Occurrence as Occ
 
 -- | Produce completions info for a file
 type instance RuleResult LocalCompletions = CachedCompletions
@@ -83,13 +85,14 @@ data CompItem = CI
   { compKind            :: CompletionItemKind
   , insertText          :: T.Text         -- ^ Snippet for the completion
   , provenance          :: Provenance     -- ^ From where this item is imported from.
-  , typeText            :: Maybe T.Text   -- ^ Available type information.
   , label               :: T.Text         -- ^ Label to display to the user.
+  , typeText            :: Maybe T.Text
   , isInfix             :: Maybe Backtick -- ^ Did the completion happen
                                    -- in the context of an infix notation.
-  , docs                :: SpanDoc        -- ^ Available documentation.
   , isTypeCompl         :: Bool
   , additionalTextEdits :: Maybe ExtendImport
+  , nameDetails         :: Maybe NameDetails -- ^ For resolving purposes
+  , isLocalCompletion   :: Bool              -- ^ Is it from this module?
   }
   deriving (Eq, Show)
 
@@ -146,3 +149,59 @@ data PosPrefixInfo = PosPrefixInfo
   , cursorPos   :: !J.Position
     -- ^ The cursor position
   } deriving (Show,Eq)
+
+
+-- | This is a JSON serialisable representation of a GHC Name that we include in
+-- completion responses so that we can recover the original name corresponding
+-- to the completion item. This is used to resolve additional details on demand
+-- about the item like its type and documentation.
+data NameDetails
+  = NameDetails Module OccName
+  deriving (Eq)
+
+-- NameSpace is abstract so need these
+nsJSON :: NameSpace -> Value
+nsJSON ns
+  | isVarNameSpace ns = String "v"
+  | isDataConNameSpace ns = String "c"
+  | isTcClsNameSpace ns  = String "t"
+  | isTvNameSpace ns = String "z"
+  | otherwise = error "namespace not recognized"
+
+parseNs :: Value -> Parser NameSpace
+parseNs (String "v") = pure Occ.varName
+parseNs (String "c") = pure dataName
+parseNs (String "t") = pure tcClsName
+parseNs (String "z") = pure tvName
+parseNs _ = mempty
+
+instance FromJSON NameDetails where
+  parseJSON v@(Array _)
+    = do
+      [modname,modid,namesp,occname] <- parseJSON v
+      mn  <- parseJSON modname
+      mid <- parseJSON modid
+      ns <- parseNs namesp
+      occn <- parseJSON occname
+      pure $ NameDetails (mkModule (stringToUnit mid) (mkModuleName mn)) (mkOccName ns occn)
+  parseJSON _ = mempty
+instance ToJSON NameDetails where
+  toJSON (NameDetails mdl occ) = toJSON [toJSON mname,toJSON mid,nsJSON ns,toJSON occs]
+    where
+      mname = moduleNameString $ moduleName mdl
+      mid = unitIdString $ moduleUnitId mdl
+      ns = occNameSpace occ
+      occs = occNameString occ
+instance Show NameDetails where
+  show = show . toJSON
+
+-- | The data that is acutally sent for resolve support
+-- We need the URI to be able to reconstruct the GHC environment
+-- in the file the completion was triggered in.
+data CompletionResolveData = CompletionResolveData
+  { itemFile :: Uri
+  , itemNeedsType :: Bool -- ^ Do we need to lookup a type for this item?
+  , itemName :: NameDetails
+  }
+  deriving stock Generic
+  deriving anyclass (FromJSON, ToJSON)
