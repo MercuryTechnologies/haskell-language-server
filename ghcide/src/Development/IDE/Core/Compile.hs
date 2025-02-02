426d7d4a93923c52daf80f1d0d48a008267c2197
Joseph Sumabat <josephrsumabat@gmail.com>
2025-02-02 03:04:13 -0500

    Fix mg error

--- ghcide/src/Development/IDE/Core/Compile.hs
+++ ghcide/src/Development/IDE/Core/Compile.hs
@@ -351,8 +351,8 @@ captureSplicesAndDeps TypecheckHelpers{..} env k = do
     -- Compute the transitive set of linkables required
     getTransitiveMods hsc_env needed_mods
 #if MIN_VERSION_ghc(9,10,0)
-      = Set.unions (Set.fromList (map moduleToNodeKey mods) : [ dep | m <- mods
-                                                              , Just dep <- fmap mkNodeKey <$> mgReachable (hsc_mod_graph hsc_env) (moduleToNodeKey m)
+      = Set.unions (Set.fromList (map moduleToNodeKey mods) : [ Set.fromList (fmap mkNodeKey deps) | m <- mods
+                                                              , Just deps <- pure $ mgReachable (hsc_mod_graph hsc_env) (moduleToNodeKey m)
                                                               ])
       where mods = nonDetEltsUniqSet needed_mods -- OK because we put them into a set immediately after
 #elif MIN_VERSION_ghc(9,3,0)
